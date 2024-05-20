39e754ee5cc7f5ec91fbf61e12d50db939406b0a
mdaadilsiddiqui <mdsiddiquiaadil@gmail.com>
2024-05-20 15:34:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// js for hamburger
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-250px";
+}
+
+// js for aadil's portfolio button 
+document.getElementById('aadil').addEventListener('click', function() {
+    window.location.href = 'https://sahill85.github.io/PortFolio/';
+}); 
